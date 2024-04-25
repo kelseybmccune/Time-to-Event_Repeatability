4bfd4b88dc58913d7c9f92cc89068c7dc92c111a
Shinichi Nakagawa <itchyshin@gmail.com>
2024-04-25 14:43:19 +1000

    Update pilot.R

--- R/pilot.R
+++ R/pilot.R
@@ -33,7 +33,10 @@ str(dat)
 coxme0 <-  coxme(Surv(LatencyFirstLand, event) ~ Condition  + (1|BirdID), data=dat)
 summary(coxme0)
 
-predict(coxme0, type = "risk")
+mu<- (mean(predict(coxme0, type = "risk")) + 0.5*0.7711779)
+
+mu*(exp(0.7711779) - 1)/(mu*(exp(0.7711779) - 1) + 1)
+0.7711779/(0.7711779 + trigamma(mu))
 
 model <- lmer(pred ~ 1 + (1|BirdID), data = dat)
 summary(model)
@@ -67,11 +70,42 @@ jsolv = read.csv(here("data", "jaySolveData.csv"))
 solv.su = coxme(Surv(Adjusted, Solve)~Treatment + (1|ID), data=jsolv)
 summary(solv.su)
 
-predict(solv.su , type = "risk")
+mu <- mean(predict(solv.su , type = "lp") + 0.5* VarCorr(solv.su)[[1]])
 
+VarCorr(solv.su)[[1]]/(VarCorr(solv.su)[[1]] + trigamma(mu))
+#########
 
+ctw <- read.csv(here("data","CTWemergence.csv"))
+# "HT" variable indicates hiding time, or the latency to emerge; "Whorls" is a visual indicator of age
+# 30 worms received 4 trials per day, across 4 days for a total of 16 trials. 
 
+# 2 individuals have NA values, but it is not explained why. I'll assume these are censored (the worm didn't emerge in the trial time)
+ctw$event <- ifelse(is.na(ctw$HT),0,1)
 
+ctw.emerg <- coxme(Surv(HT, event)~Whorls + (1|Worm_ID), data=ctw)
+summary(ctw.emerg)
+
+predict(ctw.emerg , type = "risk")
+
+mu <- exp(mean(predict(ctw.emerg , type = "lp")) + 0.5*1.030431)
+
+1.030431/(1.030431 + trigamma(mu))
+
+# criket
+
+##### Cricket time-to-emerge data ####
+crick = read.csv(here("data","CricketEmergence.csv"))
+crick$Cricket = crick$Cricket %>% str_replace(".*-", "")
+
+# data already includes a status column ("Emerge") and time
+emerg.fit = coxme(Surv(Latency.to.emerge, Emerge)~ Sex + Mass + RMR + (1|Cricket), data=crick)
+summary(emerg.fit)
+
+s2 <- VarCorr(emerg.fit)[[1]]
+
+mu <- exp(mean(predict(emerg.fit , type = "lp")) + 0.5*s2)
+
+s2/(s2 + trigamma(mu))
 
 ########
 #more tests 
